757ed54336d396842b0e42369af275c0ea22210d
hsa1pro <92202015+hsa1pro@users.noreply.github.com>
2023-03-14 16:08:53 +0700

    First Push Request

---
+++ Repositories/PostRepository/IPostRepository.cs
@@ -0,0 +1,14 @@
+﻿using Web_Social_network_BE.Models;
+
+namespace Web_Social_network_BE.Repositories.PostRepository
+{
+    public interface IPostRepository : IGeneralRepository<Post, string>
+    {
+        //Lấy hết bài đăng của người dùng có id là
+        Task<IEnumerable<Post>> GetAllAsyncByUserId(string userId);
+        //Lấy hết những người dùng đã like bài
+        Task<IEnumerable<Like>> GetAllUserLikeAsync(string postId);
+
+
+    }
+}
