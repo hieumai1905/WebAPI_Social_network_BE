50b66e59556e7a392a7df8d03f606e5f3241ca96
thaingoc04 <thainguyenngoc002@gmail.com>
2023-03-14 19:41:38 +0700

    abc

--- Repositories/CommentsRepository/ICommentRepository.cs
+++ Repositories/CommentsRepository/ICommentRepository.cs
@@ -1,8 +1,15 @@
 ﻿using Web_Social_network_BE.Models;
+using Web_Social_network_BE.Repositories;
 
-namespace Web_Social_network_BE.Repositories.CommentsRepository
+namespace Chaythurepo.Repository.CommentRepository
 {
-    public interface ICommentRepository : IGeneralRepository<Comment, long>
+    public interface ICommentRepository: IGeneralRepository<Comment, long>
     {
+        
+        Task<IEnumerable<Comment>> GetAllCommentByPostIdAsync(string postId);
+        Task<Post> AddCommnetByPostIdAsync(string postId, Comment entity);
+        Task<Post> UpdateCommentByPostIdAsync(string postId, Comment entity);
+        Task<Comment> DeleteCommentByPostIdAsync(string postId, long commentId);
+        
     }
 }
---
+++ Repositories/LikeRepository/ILikeRepository.cs
@@ -0,0 +1,13 @@
+﻿using Web_Social_network_BE.Models;
+using Web_Social_network_BE.Repositories;
+
+namespace Chaythurepo.Repository.LikeRepository
+{
+    public interface ILikeRepository: IGeneralRepository<Like, long>
+    {
+        Task<Post> AddLikeByPostIdAsync(string postId, Like entity);
+        Task<Like> DeleteLikeByPostIdAsync(string postId, long likeId);
+        Task<Comment> AddLikeByCommentIdAsync(long commentId, Like entity);
+        Task<Like> DeleteLikeByCommentIdAsync(long commentId, long likeId);
+    }
+}
