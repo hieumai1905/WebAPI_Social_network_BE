949a0a7689991e4dc0e5dc42a707db2bc0f38b3b
thaingoc04 <thainguyenngoc002@gmail.com>
2023-04-10 09:11:17 +0700

    like and comment

--- Repositories/CommentsRepository/ICommentRepository.cs
+++ Repositories/CommentsRepository/ICommentRepository.cs
@@ -3,13 +3,13 @@ using Web_Social_network_BE.Repositories;
 
 namespace Web_Social_network_BE.Repositories.CommentRepository
 {
-    public interface ICommentRepository: IGeneralRepository<Comment, long>
+    public interface ICommentRepository : IGeneralRepository<Comment, long>
     {
-        
+
         Task<IEnumerable<Comment>> GetAllCommentByPostIdAsync(string postId);
-        Task<Post> AddCommentByPostIdAsync(string postId, Comment entity);
-        Task<Post> UpdateCommentByPostIdAsync(string postId, Comment entity);
+        Task<Comment> AddCommentByPostIdAsync(string postId, Comment entity);
+        Task<Comment> UpdateCommentByPostIdAsync(string postId, Comment entity);
         Task<Comment> DeleteCommentByPostIdAsync(string postId, long commentId);
-        
+
     }
 }
